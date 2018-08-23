9f793e8dc2aff59c0e37753ce72b4f06c9aef897
Sun Yuguang <sun87959012@hotmail.com>
2018-08-23 22:34:23 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import './scss/index.scss';
 import Clock from './js/app';
 
-if (window) {
-    window.Clock = window.Clock || Clock;
-} else {
-    global.Clock = global.Clock || Clock;
-}
-
 export default Clock;
